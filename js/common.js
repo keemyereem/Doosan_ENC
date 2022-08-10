f0cf2ae76ca01956b53a08c11d986f314e745363
WooHaem <whm5020@gmail.com>
2022-08-10 15:20:29 +0900

    2022.08.10 - 브랜드페이지 작업 선행으로 인한 잠정중단

--- js/common.js
+++ js/common.js
@@ -1,7 +1,11 @@
+
+/* --------------------- DoosanENC Released 2022.08.08 --------------------- */
+/* --------------------- Published by 4m Creative --------------------- */
+
 $(function(){
     
     AOS.init({
-        // 핸들링 참고: https://github.com/michalsnik/aos
+    // 핸들링 참고: https://github.com/michalsnik/aos
 		once : true,
 		throttleDelay : 99,
 		duration: 1000,
@@ -11,20 +15,23 @@ $(function(){
 	});
 
     $('#fullpage').fullpage({
-        'verticalCentered': false,
-        'css3': false,
-        'sectionsColor': ['#FEC260', '#3FA796', '#A10035'],
-        'navigation': true,
-        'navigationPosition': 'right',
-        'navigationTooltips': ['Masterpiece', 'Business', 'Social responsibility', 'Career', 'Channel'],
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+        menu: '#rightnavi',
+        verticalCentered: false,
+        css3: true,
+        sectionsColor: ['#FEC260', '#3FA796', '#A10035', '#FEC260'],
+        // navigation: true,
+        // navigationPosition: 'right',
+        // navigationTooltips: ['Masterpiece', 'Business', 'Social responsibility', 'Career', 'Channel'],
+        // showActiveTooltip: false,
 
-        controlArrows: true,            // 슬라이드 컨트롤 애로우 생성 
-		slidesNavigation: true,         // 슬라이드 컨트롤 네비게이션 생성
-		slidesNavPosition: 'bottom',    // 슬라이드 컨트롤 네비게이션 위치 
+        // controlArrows: true,            // 슬라이드 컨트롤 애로우 생성 
+        // slidesNavigation: true,         // 슬라이드 컨트롤 네비게이션 생성
+        // slidesNavPosition: 'bottom',    // 슬라이드 컨트롤 네비게이션 위치 
 
         scrollingSpeed: 1000,
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
-        menu: '#rightnavi',
+
+
 
     });
 
@@ -74,12 +81,12 @@ var mainEvent = {
 
     intro:() => {
         $(window).load(() => {
-            var backgroundImageUrl = "../images/main/sec01_bg.png";
+            var backgroundImageUrl = "../images/main/sec01_bg1.png";
 
             // checking if image is already there in cache 
             if (sessionStorage.getItem(backgroundImageUrl)) {
 
-              console.log('intro animation start');
+              console.log('-> intro animation start');
 
               // after image showing, animation start
               setTimeout(() => {
@@ -100,7 +107,8 @@ var mainEvent = {
             // after animation ended, initializing object
             var x = document.getElementById("intro_trigger");
             x.addEventListener("animationend", () => {
-                console.log('인트로 모션 완료')
+                console.log('-> intro animation end');
+                $('.header').addClass('wht');
             });
 
 
@@ -112,6 +120,8 @@ var mainEvent = {
             spaceBetween: 30,
             speed: 1000,
             effect: "fade",
+            loop: true,
+            // autoplay: true,
             
             navigation: {
               nextEl: ".swiper-button-next",
@@ -121,73 +131,69 @@ var mainEvent = {
               el: ".swiper-pagination-sec01",
               clickable: true,
             },
+
+            //  슬라이드 이벤트 감지 - 참고 https://songsong.dev/entry/swiperjs-%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%8D%94-%EA%B8%B0%EB%B3%B8-%EC%82%AC%EC%9A%A9%EB%B2%95-%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0
+            on : {  
+
+              slideChangeTransitionStart: () => {
+                  
+                  $('.swiper-pagination-bullet').eq(this.realIndex).addClass('realIndex');
+                  // $('.swiper-pagination-bullet').not(':eq(' + this.realIndex + ')').removeClass('realIndex');
+                  
+              },
+
+              slideChangeTransitionEnd: () => {
+
+                
+              },
+            }
+
         });
 
-        $(document).on('click', '.mainSwiper .swiper-pagination-bullet', () => {
-            let $this = $('.swiper-pagination-bullet-active').position().left;
-            console.log('bullet click!');
-            
-            
 
+        let dd = $('.realIndex').position();
+        console.log(dd);
+        $('.bullet_hr').css('left', (dd / 10) + 'rem');
+        
+          
+ 
+        
+
+
+        $(document).on('click', '.mainSwiper .swiper-pagination-bullet, .mainSwiper .swiper-button', () => {
+            let $this = $('.swiper-pagination-bullet-active').position().left;
+
+            console.log('bullet click / ' + 'position : ' + $this);
+            $('.bullet_hr').css('left', ($this / 10) + 'rem')
         })
     },
 
     sec02Swiper: () => {
-        var getTimeout = function(){var e=setTimeout,b={};setTimeout=function(a,c){var d=e(a,c);b[d]=[Date.now(),c];return d};return function(a){return(a=b[a])?Math.max(a[1]-Date.now()+a[0],0):NaN}}();
+      var listArray = ["01","02","03"];
+      var swiper2 = new Swiper(".section02 .swiper", {
+        speed: 500,
+        loop: true,
+        effect: "fade",
+        watchOverflow: true,
+        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
 
-        // https://curtistimson.co.uk/post/js/default-negative-variables-to-zero-in-javascript/
-        function sanitisePercentage(i){
-          return Math.min(100,Math.max(0,i));
-        }
-        
-        // Slider
-        var percentTime;
-        var tick;
-        var progressBar = document.querySelector('.swiper-hero-progress');
-        
-        var swiper2 = new Swiper(".section02 .swiper", {
-            speed: 500,
-            loop: true,
-            effect: "fade",
-            watchOverflow: true,
-            watchSlidesProgress: true,
-            watchSlidesVisibility: true,
-
-            pagination: {
-              el: ".section02 .swiper-pagination",
-              clickable: true,
-              renderBullet: function (index, className) {
-                return '<span class="' + className + '">' +'0' + (index + 1) + '</span>';
-              },
-
-            },
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: false
-            },
-            on: {
-                init: function () {
-                  $(".swiper-progress-bar").removeClass("animate");
-                  $(".swiper-progress-bar").removeClass("active");
-                  $(".swiper-progress-bar").eq(0).addClass("animate");
-                  $(".swiper-progress-bar").eq(0).addClass("active");
-                },
-                slideChangeTransitionStart: function () {
-                  $(".swiper-progress-bar").removeClass("animate");
-                  $(".swiper-progress-bar").removeClass("active");
-                  $(".swiper-progress-bar").eq(0).addClass("active");
-                },
-                slideChangeTransitionEnd: function () {
-                  $(".swiper-progress-bar").eq(0).addClass("animate");
-                }
-            }
-
-        });
-        
+        pagination: {
+          el: '.swiper-pagination-sec02',
+          clickable: 'true',
+          type: 'bullets',
+          renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + '<em>'+ listArray[index]+'</em>' + '<i></i>' + '<b></b>'  + '</span>';
+          },
+      
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
 
 
-
-
+    });
 
     },
 
